2031b5ad1f38239b548de778314dfb98172d8801
pranto <polasahammedpranto@gmail.com>
2022-05-21 22:56:36 +0600

    footer added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 import Banner from './Banner';
 
 const Home = () => {
     return (
         <div className='max-w-7xl mx-auto px-12 mt-16'>
             <Banner />
+            <Footer />
         </div>
     );
 };

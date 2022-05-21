dbfafe074e65bacf768b4065d1820f5d2c01deec
pranto <polasahammedpranto@gmail.com>
2022-05-21 23:22:13 +0600

    reviews added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../Shared/Footer';
 import Banner from './Banner';
+import Reviews from './Reviews';
 
 const Home = () => {
     return (
         <div className='max-w-7xl mx-auto px-12 mt-16'>
             <Banner />
+            <Reviews />
             <Footer />
         </div>
     );

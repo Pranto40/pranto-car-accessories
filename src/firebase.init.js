33841db239274d67a9c62bd6006d21b6e5c1e7e5
pranto <polasahammedpranto@gmail.com>
2022-05-22 11:40:58 +0600

    require auth section added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDZz_I6Aiz-dTwvNKDeJ0nLWaXg1sbR3xY",
-  authDomain: "pranto-car-accessories.firebaseapp.com",
-  projectId: "pranto-car-accessories",
-  storageBucket: "pranto-car-accessories.appspot.com",
-  messagingSenderId: "335929929786",
-  appId: "1:335929929786:web:40a6776a26c8f18a438503"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
